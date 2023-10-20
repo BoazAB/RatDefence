720b1038c7a2ced0cdf0a8aacfffc5d086f6fa0a
Blauwberry <32714@ma-web.nl>
2023-10-20 11:35:39 +0200

    ToLongNoCommit
    changed a lot forgot to commit

--- TowerRatfence/Assets/Scripts/MovePlayer.cs
+++ TowerRatfence/Assets/Scripts/MovePlayer.cs
@@ -9,23 +9,23 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && transform.position.x > 0.9)
         {
             transform.position += new Vector3(-speed, 0) * Time.deltaTime;
             transform.rotation = Quaternion.Euler(0, 0, 0);
 
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 16)
         {
             transform.position += new Vector3(speed, 0) * Time.deltaTime;
             transform.rotation = Quaternion.Euler(0, -180, 0);
 
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && transform.position.y < 4)
         {
             transform.position += new Vector3(0, speed) * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -0.3)
         {
             transform.position += new Vector3(0, -speed) * Time.deltaTime;
         }
