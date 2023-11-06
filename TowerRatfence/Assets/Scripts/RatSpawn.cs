02d9462005084540e71922af56813781fa777a59
Blauwberry <32714@ma-web.nl>
2023-11-07 00:35:23 +0100

    made a game-over menu

--- TowerRatfence/Assets/Scripts/RatSpawn.cs
+++ TowerRatfence/Assets/Scripts/RatSpawn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RatSpawn : MonoBehaviour
 {
+    public GameObject GameOver;
     public GameObject Rat;
     [SerializeField] private int score;
     [SerializeField] private int hp = 10;
@@ -12,21 +13,34 @@ public class RatSpawn : MonoBehaviour
     {
         Instantiate(Rat);
     }
+
+    private void FixedUpdate()
+    {
+        if (hp <= 0)
+        {
+            GameOver.GetComponent<GameOverScreen>().GameOver(score);
+        }
+    }
+
     public void ReRat(GameObject whatRat, bool good)
     {
-        if (good == true)
+        if (hp > 0)
         {
-            score += 1;
-        }
-        else
-        {
-            hp -= 1;
-        }
-        if (score == 5 || score == 10 || score == 15|| score == 20|| score == 25|| score == 30 || score == 35 || score == 40 || score == 45 || score == 50)
-        {
+            if (good == true)
+            {
+                score += 1;
+            }
+            else
+            {
+                hp -= 1;
+
+            }
+            if (score == 5 || score == 10 || score == 15 || score == 20 || score == 25 || score == 30 || score == 35 || score == 40 || score == 45 || score == 50)
+            {
+                Instantiate(Rat);
+            }
             Instantiate(Rat);
         }
-        Instantiate(Rat);
         Destroy(whatRat);
     }
 }
